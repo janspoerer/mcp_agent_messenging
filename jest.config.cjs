00c7e4262e469d645d979dc957cff27c18c52459
Jan Spoerer <jan.spoerer@whu.edu>
2025-11-02 11:06:48 +0100

    fix: Simplify Jest configuration and remove duplicate config file

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,28 +1,7 @@
 module.exports = {
-  preset: 'ts-jest/presets/default-esm',
-  extensionsToTreatAsEsm: ['.ts'],
+  preset: 'ts-jest',
   testEnvironment: 'node',
-  transform: {
-    '^.+\\.tsx?$': [
-      'ts-jest',
-      {
-        useESM: true,
-        tsconfig: {
-          module: 'esnext',
-          target: 'es2020',
-        },
-      },
-    ],
-  },
-  moduleNameMapper: {
-    '^(\\.{1,2}/.*)\\.js$': '$1',
-  },
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/__tests__/**/*.ts'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  rootDir: '.',
-  collectCoverageFrom: [
-    'src/**/*.ts',
-    '!src/**/*.d.ts',
-    '!src/index.ts',
-  ],
 };
